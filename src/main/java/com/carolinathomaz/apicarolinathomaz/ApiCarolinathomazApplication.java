8cb21a68b869397c6a62dc55f7fc71ca094467aa
Carolina Thomaz <carol.com.jp@gmail.com>
2018-10-26 20:38:31 -0300

    Projeto Criado

---
+++ src/main/java/com/carolinathomaz/apicarolinathomaz/ApiCarolinathomazApplication.java
@@ -0,0 +1,12 @@
+package com.carolinathomaz.apicarolinathomaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarolinathomazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarolinathomazApplication.class, args);
+	}
+}
---
+++ src/test/java/com/carolinathomaz/apicarolinathomaz/ApiCarolinathomazApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carolinathomaz.apicarolinathomaz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCarolinathomazApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

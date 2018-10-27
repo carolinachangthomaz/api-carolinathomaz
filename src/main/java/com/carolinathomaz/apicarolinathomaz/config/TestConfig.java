e049f55cc15acef3d8318197a11ff39251ddb882
Carolina Thomaz <carol.com.jp@gmail.com>
2018-10-26 23:14:59 -0300

    Profile Service

---
+++ src/main/java/com/carolinathomaz/apicarolinathomaz/config/TestConfig.java
@@ -0,0 +1,14 @@
+package com.carolinathomaz.apicarolinathomaz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	public TestConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

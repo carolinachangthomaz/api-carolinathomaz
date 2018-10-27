94d8ce7c48c5398f0bf38c2e5824b015b0fd515d
Carolina Thomaz <carol.com.jp@gmail.com>
2018-10-27 00:06:00 -0300

    DBService - insstanciando o BD / TestConfig - profile de teste inicia instanciando o BD

--- src/main/java/com/carolinathomaz/apicarolinathomaz/config/TestConfig.java
+++ src/main/java/com/carolinathomaz/apicarolinathomaz/config/TestConfig.java
@@ -1,14 +1,28 @@
 package com.carolinathomaz.apicarolinathomaz.config;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.carolinathomaz.apicarolinathomaz.services.DBService;
+
+// profile para teste instanciando o banco de dados
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
-	public TestConfig() {
-		// TODO Auto-generated constructor stub
+	@Autowired
+	private DBService dbService;
+	
+	
+	@Bean
+	public boolean instantiateDattabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
 	}
 
 }
---
+++ src/main/java/com/carolinathomaz/apicarolinathomaz/services/DBService.java
@@ -0,0 +1,90 @@
+package com.carolinathomaz.apicarolinathomaz.services;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carolinathomaz.apicarolinathomaz.model.CentroDistribuicao;
+import com.carolinathomaz.apicarolinathomaz.model.Cidade;
+import com.carolinathomaz.apicarolinathomaz.model.Cliente;
+import com.carolinathomaz.apicarolinathomaz.model.Embalagem;
+import com.carolinathomaz.apicarolinathomaz.model.EnderecoCentroDistribuicao;
+import com.carolinathomaz.apicarolinathomaz.model.Estado;
+import com.carolinathomaz.apicarolinathomaz.repositories.CentroDistribuicaoRepository;
+import com.carolinathomaz.apicarolinathomaz.repositories.CidadeRepository;
+import com.carolinathomaz.apicarolinathomaz.repositories.ClienteRepository;
+import com.carolinathomaz.apicarolinathomaz.repositories.EmbalagemRepository;
+import com.carolinathomaz.apicarolinathomaz.repositories.EnderecoCentroDistribuicaoRepository;
+import com.carolinathomaz.apicarolinathomaz.repositories.EstadoRepository;
+
+//Service criado para popular o banco de dados
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EmbalagemRepository embalagemRepository;
+	
+	@Autowired
+	private CentroDistribuicaoRepository centroDistribuicaoRepository;
+	
+	@Autowired
+	private EnderecoCentroDistribuicaoRepository enderecoCentroDistribuicaoRepository;
+
+	public void instantiateTestDatabase() throws ParseException {
+		
+		Cliente cliente1 = new Cliente(null, "Banco do Brasil", "contato@bb.com.br");
+		Cliente cliente2 = new Cliente(null, "Itaú", "contato@itau.com.br");
+		
+		clienteRepository.saveAll(Arrays.asList(cliente1,cliente2));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		
+		Embalagem embalagem1 = new Embalagem(null, "Caixa", new BigDecimal("10"));
+		Embalagem embalagem2 = new Embalagem(null, "Envelope", new BigDecimal("2"));
+		
+		embalagemRepository.saveAll(Arrays.asList(embalagem1,embalagem2));
+		
+		CentroDistribuicao centroDistribuicao1 = new CentroDistribuicao(null, "CD 1");
+		CentroDistribuicao centroDistribuicao2 = new CentroDistribuicao(null, "CD 2");
+		
+		centroDistribuicaoRepository.saveAll(Arrays.asList(centroDistribuicao1,centroDistribuicao2));
+		
+		EnderecoCentroDistribuicao ecd1 = new EnderecoCentroDistribuicao(null, "Rua JoãoFranco Oliveira", "91", "comercial", "campininha", "04678100", centroDistribuicao1, cidade1);
+		EnderecoCentroDistribuicao ecd2 = new EnderecoCentroDistribuicao(null, "Rua Pinheiros", "100", "comercial", "sto Amaro", "45564565", centroDistribuicao1, cidade2);
+		EnderecoCentroDistribuicao ecd3 = new EnderecoCentroDistribuicao(null, "AV Faria Lima", "91", "comercial", "sto Amaro", "45564565", centroDistribuicao2, cidade2);
+		
+		enderecoCentroDistribuicaoRepository.saveAll(Arrays.asList(ecd1,ecd2,ecd3));
+		
+		centroDistribuicao1.getEnderecos().addAll(Arrays.asList(ecd1,ecd2));
+		centroDistribuicao2.getEnderecos().addAll(Arrays.asList(ecd3));
+		
+		centroDistribuicaoRepository.saveAll(Arrays.asList(centroDistribuicao1,centroDistribuicao2));
+		
+		
+	}
+
+}
